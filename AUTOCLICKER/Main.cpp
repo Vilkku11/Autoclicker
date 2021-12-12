c1fbaf00f5fb44b4bf1af4eafc23ae08b92f4b4e
William Reima <william.reima@gmail.com>
2021-12-12 20:05:21 +0200

    Add project files.

---
+++ AUTOCLICKER/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <windows.h>
+
+
+
+int main() {
+
+	std::cout << " z on x off q to quit " << std::endl;
+
+	
+
+
+	bool autoclicker = false;
+	bool exit = false;
+	
+	while (exit == false) {
+
+		if (GetAsyncKeyState('Z')) {
+			autoclicker = true;
+
+		}
+		else if (GetAsyncKeyState('X')) {
+
+			autoclicker = false;
+		}
+		else if (GetAsyncKeyState('Q')) {
+			exit = true;
+		}
+
+		if (autoclicker == true) {
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			
+			Sleep(1);
+			
+		}
+
+
+
+
+	}
+
+	return 0;
+}
